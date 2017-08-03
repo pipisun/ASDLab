8ce4a73caa5dd3d47673b91d801c2bc4e3cc2640
Administrator <Administrator@WIN7-1706051100>
2017-08-03 11:36:41 +0800

    Strategy pattern inited.

---
+++ src/strategy/Model.java
@@ -0,0 +1,10 @@
+package strategy;
+
+import java.util.List;
+
+public interface Model {
+	public int BASETICKETPRICE = 300;
+	public int COST = 50000;
+
+	public long getRevenue(List<Flight> flights);
+}
---
+++ src/strategy/ModelProcess.java
@@ -0,0 +1,15 @@
+package strategy;
+
+import java.util.List;
+
+public class ModelProcess {
+	private Model model;
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	public long getRevenue(List<Flight> flights) {
+		return model.getRevenue(flights);
+	}
+}
