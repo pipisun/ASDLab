81d577439da17b83fa723c7f0c58d01dc51a36d4
Administrator <Administrator@WIN7-1706051100>
2017-08-06 10:33:43 +0800

    Updated

--- src/interpreter/ExpressionMain.java
+++ src/interpreter/ExpressionMain.java
@@ -2,13 +2,13 @@ package interpreter;
 
 public class ExpressionMain {
 	public static void main(String[] args) {
-		//return 9
+		// return 9
 		String tokenString = "- + 10 5 - 8 2";
 		Expression expression = new Expression(tokenString);
-		System.out.println(expression.interpret()); 
-		//return 90
+		System.out.println(expression.interpret());
+		// return 90
 		String tokenString2 = "* + 10 5 - 8 2";
 		Expression expression2 = new Expression(tokenString2);
-		System.out.println(expression2.interpret()); 
-		}
+		System.out.println(expression2.interpret());
+	}
 }
